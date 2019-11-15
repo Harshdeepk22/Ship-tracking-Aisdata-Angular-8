7c704a0e9b0946a989fb69ba92480b9d828f4225
Harsh <hkaur@contriverz.com>
2019-11-15 17:18:37 +0530

    Fix issues

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/Shiptracking'));
 
 app.get('/*', function(req,res) {
     
